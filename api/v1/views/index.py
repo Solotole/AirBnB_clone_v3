453a9bfbf72d705d5a0a96e142b391884d0f7200
Solotole <solomontoleak47@gmail.com>
2024-06-07 13:36:17 +0100

    count of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,13 @@ index
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
@@ -16,3 +23,17 @@ def status():
     }
     resp = jsonify(data)
     return resp
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def statistics():
+    """ method to return count in dictionary form """
+    response = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(response)
