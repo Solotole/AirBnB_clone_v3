a3e2f10b1c8af44a4adc5ea7ae1ce360271c6e92
Solotole <solomontoleak47@gmail.com>
2024-06-09 11:56:48 +0100

    amenities REST

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """module defining routes for amenity class
 """
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -13,10 +13,10 @@ classes = {"Amenity": Amenity}
 def getting_all_objects():
     """ getting a list of all objects """
     dict_amenity = {}
-    dict_amenity = storage.all(classes['Amenity'])
+    dict_amenity = storage.all(classes['Amenity']).all()
     all_amenity = []
     if dict_amenity:
-        all_amenity.append(dict_amenity)
+        all_amenity.append(dict_amenity.to_dict())
     return jsonify(all_amenity)
 
 
@@ -43,7 +43,7 @@ def deleting_amenity_object(amenity_id):
     if amenity_object:
         storage.delete(amenity_object)
         storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['POST'],
@@ -57,7 +57,7 @@ def posting_new_object(amenity_id):
         abort(400, description='Misiing name')
     amenity_instance = classes['Amenity'](**amenity_object)
     amenity_instance.save()
-    return jsonify(amenity_instance.to_dict()), 201
+    return make_response(jsonify(amenity_instance.to_dict()), 201)
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
@@ -76,4 +76,4 @@ def putting_amenity_object(amenity_id):
         if keys not in ignore_list:
             setattr(existing_object, keys, values)
     storage.save()
-    return jsonify(existing_object.to_dict()), 200
+    return make_response(jsonify(existing_object.to_dict()), 200)
