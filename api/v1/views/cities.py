b1b642ee5534eb2c2bec57d33a2957c297e7a5f2
Solotole <solomontoleak47@gmail.com>
2024-06-09 13:02:19 +0100

    city REST

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" city REST API module definition """
+""" city REST API module definition
+"""
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
