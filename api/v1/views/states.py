ea8c7ef5d152d902a1717bad509df74bda79dd63
Solotole <solomontoleak47@gmail.com>
2024-06-10 10:03:18 +0100

    state view

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,27 +13,26 @@ def define_state():
     """
     return the list of states
     """
-    state_list = []
-    state_obj = s.all(State)
-    for obj in state_obj.values():
-        state_list.append(obj.to_dict())
-    return jsonify(state_list)
+    states = []
+    states = s.all(State)
+    for obj in states.values():
+        states.append(obj.to_dict())
+    return jsonify(states)
 
 
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def creat_state():
     """
     create state route
-    :return: newly created state obj
     """
-    state_json = request.get_json(silent=True)
-    if state_json is None:
+    state_request = request.get_json(silent=True)
+    if state_request is None:
         abort(400, description='Not a JSON')
-    if "name" not in state_json:
+    if "name" not in state_request:
         abort(400, description='Missing name')
-    new_state = State(**state_json)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    new_state_object = State(**state_request)
+    new_state_object.save()
+    return jsonify(new_state_object.to_dict()), 201
 
 
 @app_views.route("/states/<state_id>",  methods=["GET"], strict_slashes=False)
@@ -43,10 +42,10 @@ def state_by_id(state_id):
     :param state_id: state object id
     :return: state obj with the specified id or error
     """
-    fetched_obj = s.get(State, state_id)
-    if not fetched_obj:
+    fetched = s.get(State, state_id)
+    if not fetched:
         abort(404)
-    return jsonify(fetched_obj.to_dict())
+    return jsonify(fetched.to_dict())
 
 
 @app_views.route("/states/<state_id>",  methods=["PUT"], strict_slashes=False)
@@ -54,7 +53,7 @@ def state_put(state_id):
     """
     updates specific State object by ID
     """
-    state_json = request.get_json(silent=True)
+    state_request = request.get_json(silent=True)
     if not request.get_json():
         abort(400, description='Not a JSON')
     fetched_obj = s.get(State, state_id)
