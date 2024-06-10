8698aad70c85dc2a5e5bfb4603ce07f542cb5a44
Solotole <solomontoleak47@gmail.com>
2024-06-10 15:38:32 +0100

    securing password

--- models/base_model.py
+++ models/base_model.py
@@ -62,7 +62,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if models.storage_t == 'db':
-            new_dict.pop('passsword', None)
+            del new_dict['passsword']
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
